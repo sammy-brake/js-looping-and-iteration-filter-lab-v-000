51e06ddca92aa42a81362948ba02ddc43d29c9bd
Samantha brake <sammy.brake@gmail.com>
2019-05-17 03:37:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
   return drivers.filter(function(driver){
-    return driver.toLowerCase() == string
+    return driver.toLowerCase() == string.toLowerCase()
   });
 };
